e9bba275022a2ff7cd2d9dac5cdc7a45c662b4cf
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-04-03 23:12:09 -0300

    att

--- Matrizes.py
+++ Matrizes.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import math
+from turtle import *
 
 #===================================================
 #
@@ -15,10 +16,23 @@ import math
 #
 #====================================================
 
-Inicio = Tk()
-Inicio.title("Calculadora Matrizes")
-Inicio.geometry("600x500+400+100")
-Inicio.configure(bg='red')
+inicio = Tk()
+inicio.title("Calculadora Matrizes")
+inicio.geometry("600x500+400+100")
+inicio['bg'] = '#C58282'
+
+widget1 = Frame(inicio)
+widget1.grid(ipadx=140)
+widget1['bg'] = '#C58282'
+widget2 = Frame(inicio)
+widget2.grid()
+widget2['bg'] = '#C58282'
+
+h1 = Label(widget1, text = "Escolha Calculo", foreground='white')
+h1['bg'] = '#C58282'
+h1["font"] = ("Inter", "32")
+
+bmult = Button(widget2, text="Multiplicar")
 
 #===================================================
 #
@@ -26,8 +40,9 @@ Inicio.configure(bg='red')
 #
 #====================================================
 
-
-
+h1.pack()
+bmult.pack()
+mainloop()
 #===================================================
 #
 #                      Console
