92e7350e17c22809f3a64a7e2d6bdac9fd578d2a
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-04-03 23:18:00 -0300

    att

--- Matrizes.py
+++ Matrizes.py
@@ -1,6 +1,5 @@
 from tkinter import *
 import math
-from turtle import *
 
 #===================================================
 #
@@ -22,17 +21,30 @@ inicio.geometry("600x500+400+100")
 inicio['bg'] = '#C58282'
 
 widget1 = Frame(inicio)
-widget1.grid(ipadx=140)
+widget1.grid(ipadx=180)
 widget1['bg'] = '#C58282'
+
 widget2 = Frame(inicio)
 widget2.grid()
 widget2['bg'] = '#C58282'
 
+widget3 = Frame(inicio)
+widget3.grid()
+widget3['bg'] = '#C58282'
+
 h1 = Label(widget1, text = "Escolha Calculo", foreground='white')
 h1['bg'] = '#C58282'
-h1["font"] = ("Inter", "32")
+h1["font"] = ("Inter", "25")
 
 bmult = Button(widget2, text="Multiplicar")
+
+h2 = Label(widget3, text = "Tipo Matriz X", foreground='white')
+h2['bg'] = '#C58282'
+h2["font"] = ("Inter", "25")
+
+h3 = Label(widget3, text = "Tipo Matriz Y", foreground='white')
+h3['bg'] = '#C58282'
+h3["font"] = ("Inter", "25")
 
 #===================================================
 #
@@ -42,7 +54,10 @@ bmult = Button(widget2, text="Multiplicar")
 
 h1.pack()
 bmult.pack()
+h2.pack(side=LEFT)
+h3.pack(side=LEFT)
 mainloop()
+
 #===================================================
 #
 #                      Console
